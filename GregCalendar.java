fc163ab863d33f8fb52f8c02b2a5a0da446c22a0
iTwicKz <twickes32@gmail.com>
2014-12-06 11:00:49 -0500

    First additions of Code

---
+++ GregCalendar.java
@@ -0,0 +1,17 @@
+import java.util.GregorianCalendar;
+
+class Exercise_9_5 {
+	public static void main(String[] args){
+		
+		
+		GregorianCalendar calendar = new GregorianCalendar();		
+		System.out.println("Year: " + calendar.get(GregorianCalendar.YEAR));
+		
+		
+		
+		
+		
+		
+	}
+	
+}
