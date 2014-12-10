37b98e427d8cde0d80cd3547d6a72dfe32b9e094
iTwicKz <twickes32@gmail.com>
2014-12-09 22:18:34 -0500

    Finished Dat Exam

---
+++ Circle.java
@@ -0,0 +1,11 @@
+
+class Circle {
+	private double radius = 10;
+	
+	
+	
+	public static void main(String[] args){
+		Circle a = new Circle();
+		System.out.println(a.radius);
+	}
+}
--- ClassTester.java
+++ ClassTester.java
@@ -1,11 +1,21 @@
 import java.util.Random;
 public class ClassTester {
 	public static void main(String[] args){
-	Random random = new Random(3);
+	/*Random random = new Random(3);
 	for(int i = 0; i < 12; i++){
 	System.out.print(random.nextInt(1000));}
 	Random random2 = new Random(3);
 	System.out.println();
 	for(int i = 0; i < 12; i++){
-	System.out.print(random2.nextInt(1000));}
+	System.out.print(random2.nextInt(1000));} */
+	/*
+	Random rand = new Random();
+	int x = rand.nextInt(3)+7;
+	System.out.println(x);*/
+		/*
+		String str = "";
+		for(char ch = 'a'; ch < 'z'; ch++){
+			str = str + ch;
+		}
+		System.out.println(str);
 }}
